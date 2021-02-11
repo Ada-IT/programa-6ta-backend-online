18b9439172b6c938952c710ffb7e4e150d6b5eeb
ShadowDrone <shadowdrone@hotmail.com>
2021-02-11 09:41:04 -0300

    fix coma

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     Introducción: ['00-intro'],
     'Módulo 1 - Introducción a la programación y Programación Estructurada': [
       'modulo-1/clase-01',
-      'modulo-1/clase-02',
+      'modulo-1/clase-02'
     ],
   },
 }
